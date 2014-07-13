0a11801e45be77af3c3249bf96e02e8c3db06546
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-13 16:37:21 +0200

    Add build

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 
-require("./index.js");
+var transitions = require("./index.js");
+
+if (!(transitions instanceof Array)) throw new Error("transitions is not an Array.");
+
+if (transitions.length === 0) throw new Error("transitions is empty.");
 
